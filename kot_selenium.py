a676a7d6a19b36948c6c5d0a59902268aa7ce54c
hwakabh <hrykwkbys1024@gmail.com>
2024-05-09 18:57:59 +0900

    feat: initial release.

--- kot_selenium.py
+++ kot_selenium.py
@@ -1,1 +1,88 @@
-print('hello');
+import os
+import sys
+import random
+from time import sleep
+from typing import Literal
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+# constants
+KOT_URL = "https://s3.ta.kingoftime.jp/independent/recorder2/personal"
+MAX_UI_WAIT_TIME_SEC = 10
+
+KOT_OPS = os.environ.get('KOT_OPS')
+KOT_USERNAME = os.environ.get('KOT_USERNAME')
+KOT_PASSWORD = os.environ.get('KOT_PASSWORD')
+
+# randomize wait time
+START_OFFSET_MIN = random.randint(0, 60)
+
+
+print('>>> Precheck for user input')
+if (KOT_OPS is None) or (KOT_USERNAME is None) or (KOT_PASSWORD is None):
+    print('Required environmental variable(s) not provided, please set: KOT_OPS, KOT_USERNAME, KOT_PASSWORD')
+    sys.exit(1)
+
+if KOT_OPS.lower() not in ['clock-in', 'clock-out']:
+    print('provided operation could not be accepted, only `clock-in` or `clock-out` can be used for KOT_OPS.')
+    sys.exit(1)
+
+print(f"KOT_OPS: {KOT_OPS}")
+print(f"KOT_USERNAME: {KOT_USERNAME}")
+print(f"KOT_PASSWORD: ******\n")
+
+print(f'>>> Waiting for {START_OFFSET_MIN} mintues to start, maximum wait time is 60 minutes.')
+sleep(START_OFFSET_MIN * 60)
+print('OK, starting to Selenium operation\n')
+
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+
+print('>>> Instantiating Webdriver and its utilities...')
+driver = webdriver.Chrome(options=chrome_options)
+wait = WebDriverWait(driver, MAX_UI_WAIT_TIME_SEC)
+
+try:
+    print('>>> Login and wait until page rendered')
+    driver.get(KOT_URL)
+    wait.until(expected_conditions.title_contains("My Recorder"))
+    sleep(5)
+except Exception as e:
+    print('Failed to open URL.')
+    print(e)
+    sys.exit(1)
+
+try:
+    print('>>> Fillout ID and Password for login')
+    element_id = driver.find_element(By.ID, 'id')
+    element_id.send_keys(KOT_USERNAME)
+    element_password = driver.find_element(By.ID, 'password')
+    element_password.send_keys(KOT_PASSWORD)
+
+    element_ok = driver.find_element(By.CLASS_NAME, 'btn-control-message').click()
+except Exception as e:
+    print('Failed to login KOT.')
+    print(e)
+    sys.exit(1)
+
+# Wait and click button for clock-in/clock-out
+sleep(5)
+
+try:
+    print('>>> Registering the entries')
+    if KOT_OPS.lower() == 'clock-in':
+        element_clockin = driver.find_element(By.CLASS_NAME, 'record-clock-in').click()
+    elif KOT_OPS.lower() == 'clock-out':
+        element_clockout = driver.find_element(By.CLASS_NAME, 'record-clock-out').click()
+except Exception as e:
+    print('Failed to click clock-in/clock-out button')
+    sys.exit(1)
+
+sleep(5)
+driver.quit()
+
+print(f'Done, completed daily [ {KOT_OPS} ] operations.\n')
--- ~README.md
+++ ~README.md
@@ -1,1 +1,33 @@
 # kot_selenium
+To Be Updated
+
+## Configure with Linux Cron
+To Be Updated
+
+## Run locally
+Note that if you need to run program from your laptop, please confirm that Python 3.x is installed onto your system.
+As initial release, Python 3.11.8 (virtualenv) has been tested.
+
+```shell
+# Clock-In
+% export KOT_OPS='clock-in'
+% export KOT_USERNAME='xxxxxxx'
+% export KOT_PASSWORD='xxxxxxx'
+
+# Run
+# Note that password is hidden in stdout and the following outputs are omitted as examples
+% python kot_selenium.py
+>>> Precheck for user input
+KOT_OPS: clock-out
+KOT_USERNAME: trme3c3382413
+KOT_PASSWORD: ******
+
+>>> Waiting for 3 mintues to start, maximum wait time is 60 minutes.
+OK, starting to Selenium operation
+
+>>> Instantiating Webdriver and its utilities...
+>>> Login and wait until page rendered
+>>> Fillout ID and Password for login
+>>> Registering the entries
+Done, completed daily [ clock-in ] operations.
+```
